45bddd535a78fbbe7ed5d0e82a74860c974e7503
Julio-Cesar-Flores <jfloreslo@gmail.com>
2022-08-15 17:40:28 -0700

    Agregado express

---
+++ api/api.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const api = express();
+
+api.get("/status", (req, res) => {
+  return res.json({
+    msg: "API en línea y funcionando",
+  });
+});
+
+export default api;
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import http from "http"; //http es nativo de node
+import api from "./api/api.js";
+
 const port = 3000;
-const server = http.createServer();
+//Aqui ponemos la instancia de express
+const server = http.createServer(api);  
 
 server.listen(port);
 
